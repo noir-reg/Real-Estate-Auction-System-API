ebd270d22aa020b811412cd63351c11f63a2fb52
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-02-21 20:01:36 +0700

    implemented crud for staff

---
+++ Real-Estate-Auction-System-API/Controllers/StaffController.cs
@@ -0,0 +1,81 @@
+﻿using BusinessObjects.Dtos.Request;
+using BusinessObjects.Dtos.Response;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Real_Estate_Auction_System_API.Controllers;
+
+[ApiController]
+[Route("api/staffs")]
+public class StaffController : ControllerBase
+{
+    private readonly IStaffService _staffService;
+
+    public StaffController(IStaffService staffService)
+    {
+        _staffService = staffService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ListResponseBaseDto<StaffListResponseDto>>> GetList([FromQuery] StaffQuery request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var data = await _staffService.GetStaffsAsync(request);
+        return Ok(data);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<StaffDetailResponseDto>> GetDetail([FromRoute] Guid id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var data = await _staffService.GetStaffAsync(x => x.UserId == id);
+        return Ok(data);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] AddStaffRequestDto request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        await _staffService.AddStaffAsync(request);
+        return Ok("Create successfully");
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromBody] UpdateStaffRequestDto request, [FromRoute] Guid id)
+    {
+        request.StaffId = id;
+        
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        await _staffService.UpdateStaffAsync(request);
+        return Ok("Update successfully");
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        await _staffService.DeleteStaffAsync(id);
+        return Ok("Delete successfully");
+    }
+    
+}
---
+++ Services/IStaffService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using BusinessObjects.Dtos.Request;
+using BusinessObjects.Dtos.Response;
+using BusinessObjects.Entities;
+
+namespace Services;
+
+public interface IStaffService
+{
+    Task<ListResponseBaseDto<StaffListResponseDto>> GetStaffsAsync(StaffQuery request);
+    Task AddStaffAsync(AddStaffRequestDto request);
+    Task UpdateStaffAsync(UpdateStaffRequestDto request);
+    Task<StaffDetailResponseDto?> GetStaffAsync(Expression<Func<Staff,bool>> predicate);
+    Task DeleteStaffAsync(Guid id);
+}
