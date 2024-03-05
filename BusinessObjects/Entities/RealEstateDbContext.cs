e4b35be5413b6dae700d2c80247c7559ea7c4094
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-05 18:47:36 +0700

    bug fix

--- BusinessObjects/Entities/RealEstateDbContext.cs
+++ BusinessObjects/Entities/RealEstateDbContext.cs
@@ -40,7 +40,7 @@ public class RealEstateDbContext : DbContext
             .SetBasePath(Directory.GetCurrentDirectory())
             .Build();
 
-        return config.GetConnectionString("LocalConnection");
+        return config.GetConnectionString("DefaultConnection");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
